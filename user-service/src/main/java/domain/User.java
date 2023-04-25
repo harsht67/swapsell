b8ff5e8ed13e70251c4f9cbf599d63d6c2319ff9
vishal <vishal88kh@gmail.com>
2023-04-25 09:43:15 +0530

    declared all the fields for User

--- user-service/src/main/java/domain/User.java
+++ user-service/src/main/java/domain/User.java
@@ -1,7 +1,20 @@
 package domain;
 
-import java.lang.annotation.Documented;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
 
+@Document
 public class User {
+    @MongoId
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private String image;
+
+
 }
