5de5de084e32eee241025512c7c5500476fad7ad
vishal <vishal88kh@gmail.com>
2023-04-25 10:30:41 +0530

    added userService as dependency injection and annotated with @Autowired

--- user-service/src/main/java/com/stackroute/userservice/controller/UserServiceController.java
+++ user-service/src/main/java/com/stackroute/userservice/controller/UserServiceController.java
@@ -1,7 +1,14 @@
 package com.stackroute.userservice.controller;
 
+import com.stackroute.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserServiceController {
+    private final UserService userService;
+    @Autowired
+    public UserServiceController(UserService userService) {
+        this.userService = userService;
+    }
 }
