9d4f18e43bc190c2bec3a1b0c1106ca76b430d7a
vishal <vishal88kh@gmail.com>
2023-04-25 10:03:44 +0530

    added userServiceRepository as dependency injection

--- user-service/src/main/java/com/stackroute/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/stackroute/userservice/service/UserServiceImpl.java
@@ -2,12 +2,22 @@ package com.stackroute.userservice.service;
 
 import com.stackroute.userservice.domain.User;
 import com.stackroute.userservice.exception.UserAlreadyExistsException;
+import com.stackroute.userservice.repository.UserServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserServiceRepository userServiceRepository;
+    @Autowired
+    public UserServiceImpl(UserServiceRepository userServiceRepository) {
+        this.userServiceRepository = userServiceRepository;
+    }
+
     @Override
     public User registerUserToApplication(User userDetails) throws UserAlreadyExistsException {
+
         return null;
     }
 }
