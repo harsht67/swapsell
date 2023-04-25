f55bc1c7445251ccc45363ae98aeb6e3cf0e880c
vishal <vishal88kh@gmail.com>
2023-04-25 10:33:49 +0530

    added a method to map the user with "/registerUser"

--- user-service/src/main/java/com/stackroute/userservice/controller/UserServiceController.java
+++ user-service/src/main/java/com/stackroute/userservice/controller/UserServiceController.java
@@ -1,7 +1,13 @@
 package com.stackroute.userservice.controller;
 
+import com.stackroute.userservice.domain.User;
+import com.stackroute.userservice.exception.UserAlreadyExistsException;
 import com.stackroute.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +16,15 @@ public class UserServiceController {
     @Autowired
     public UserServiceController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/registerUser")
+    public ResponseEntity<?> registerUser(@RequestBody User user){
+        try {
+            User userDetails = userService.registerUserToApplication(user);
+            return new ResponseEntity<>(userDetails,HttpStatus.CREATED);
+        } catch (UserAlreadyExistsException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        }
     }
 }
