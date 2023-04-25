f2b156256033d272aed2f79b6f9a300576c4c8b7
vishal <vishal88kh@gmail.com>
2023-04-25 09:52:07 +0530

    removed Service package

--- user-service/src/main/java/com/stackroute/userservice/service/UserServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.stackroute.userservice.service;
-
-import com.stackroute.userservice.domain.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserServiceRepository extends MongoRepository<User,Long> {
-}
