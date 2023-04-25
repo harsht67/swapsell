e49602012af4f54a64c1333421a75f1051586c0f
vishal <vishal88kh@gmail.com>
2023-04-25 10:28:52 +0530

    created a method to add register a user in mongoDB database

--- user-service/src/main/java/com/stackroute/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/stackroute/userservice/service/UserServiceImpl.java
@@ -6,18 +6,33 @@ import com.stackroute.userservice.repository.UserServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserServiceRepository userServiceRepository;
+
     @Autowired
     public UserServiceImpl(UserServiceRepository userServiceRepository) {
         this.userServiceRepository = userServiceRepository;
     }
 
+    boolean checkExistingUser(User user) throws UserAlreadyExistsException {
+        Optional<User> userByEmail = userServiceRepository.findUserByEmail(user.getEmail());
+        boolean userNotExists = true;
+        if (userByEmail.isPresent()) {
+            throw new UserAlreadyExistsException("This email id is already taken try other");
+        }
+        return userNotExists;
+    }
+
     @Override
     public User registerUserToApplication(User userDetails) throws UserAlreadyExistsException {
-
-        return null;
+        User storeUserDetails =null;
+        if (checkExistingUser(userDetails)) {
+            storeUserDetails= userServiceRepository.save(userDetails);
+        }
+        return storeUserDetails;
     }
 }
