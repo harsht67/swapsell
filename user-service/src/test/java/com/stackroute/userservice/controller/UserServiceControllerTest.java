b5625664a920c209c8c9b49bb41fff7cc478aed7
vishal <vishal88kh@gmail.com>
2023-04-25 15:12:11 +0530

    created a test for controller

---
+++ user-service/src/test/java/com/stackroute/userservice/controller/UserServiceControllerTest.java
@@ -0,0 +1,22 @@
+package com.stackroute.userservice.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void registerUser() {
+    }
+}
