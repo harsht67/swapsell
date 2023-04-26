314a492903bf897a4ee2af03aca85b1823979a20
vishal <vishal88kh@gmail.com>
2023-04-26 16:25:46 +0530

    implemented logic to generate random values

--- user-service/src/main/java/com/stackroute/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/stackroute/userservice/service/UserServiceImpl.java
@@ -11,11 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserServiceRepository userServiceRepository;
+    private static final Random random = new Random();
 
     @Autowired
     public UserServiceImpl(UserServiceRepository userServiceRepository) {
@@ -26,16 +28,34 @@ public class UserServiceImpl implements UserService {
         Optional<User> userByEmail = userServiceRepository.findUserByEmail(user.getEmail());
         return userByEmail.isPresent();
     }
+    public static long generateId() {
+        long id = random.nextLong();
+        if (id == Long.MIN_VALUE) {
+            id = 0L; // To avoid negative value for id
+        } else {
+            id = Math.abs(id);
+        }
+        return id;
+    }
     @RabbitListener(queues = MessageConfiguration.queueName1)
-    public void userDataFromAuthService(UserDTO userDTO){
+    public void userDataFromAuthService(UserDTO userDTO) throws UserAlreadyExistsException {
         System.out.println(userDTO);
+        registerUserToApplication(userDTO);
     }
     @Override
-    public User registerUserToApplication(User userDetails) throws UserAlreadyExistsException {
-        if (checkExistingUser(userDetails)){
+    public User registerUserToApplication(UserDTO userDTO) throws UserAlreadyExistsException {
+        User user = new User();
+        user.setId(generateId());
+        user.setEmail(userDTO.getEmail());
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        user.setPassword(userDTO.getPassword());
+        System.out.println(user);
+        if (checkExistingUser(user)){
             throw new UserAlreadyExistsException("This email id is already taken other");
         }
-        return userServiceRepository.save(userDetails);
+        return userServiceRepository.save(user);
+
     }
 
     @Override
