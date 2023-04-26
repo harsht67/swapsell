af7e36dd1a761a266edf0ac65349589e70be0761
vishal <vishal88kh@gmail.com>
2023-04-26 14:32:20 +0530

    added fields for UserDTO

--- authentication-service/src/main/java/com/stackroute/authenticationservice/configuration/UserDTO.java
+++ authentication-service/src/main/java/com/stackroute/authenticationservice/configuration/UserDTO.java
@@ -10,4 +10,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
 }
