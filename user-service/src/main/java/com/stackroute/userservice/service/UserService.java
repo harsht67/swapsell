6a8ef54b2aba3947a56c449f15d039f8941f7767
vishal <vishal88kh@gmail.com>
2023-04-26 16:24:55 +0530

    changed parameter to UserDTO

--- user-service/src/main/java/com/stackroute/userservice/service/UserService.java
+++ user-service/src/main/java/com/stackroute/userservice/service/UserService.java
@@ -1,11 +1,12 @@
 package com.stackroute.userservice.service;
 
+import com.stackroute.userservice.configuration.UserDTO;
 import com.stackroute.userservice.domain.User;
 import com.stackroute.userservice.exception.UserAlreadyExistsException;
 import com.stackroute.userservice.exception.UserNotFoundException;
 
 public interface UserService {
-    User registerUserToApplication(User userDetails) throws UserAlreadyExistsException;
+    User registerUserToApplication(UserDTO userDTO) throws UserAlreadyExistsException;
     User updateUserDetails(User userDetails) throws UserNotFoundException;
 
     //TODO : implement delete method
