cdcaf882a9f37fdad5fbb03a6bf8a81433f5f2b2
vishal <vishal88kh@gmail.com>
2023-04-27 00:22:08 +0530

    added filter registration bean

--- user-service/src/main/java/com/stackroute/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/stackroute/userservice/UserServiceApplication.java
@@ -1,13 +1,23 @@
 package com.stackroute.userservice;
 
+import com.stackroute.userservice.filter.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
+	}
+	@Bean
+	public FilterRegistrationBean<JwtFilter> filterRegistrationBean(){
+		FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("swapSell/user/*");
+		return registrationBean;
 	}
 
 }
