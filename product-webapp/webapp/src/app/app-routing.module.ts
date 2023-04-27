f0919c7dec661de6f49cc37a76ebf20c2839de35
vishal <vishal88kh@gmail.com>
2023-04-27 14:01:36 +0530

    added routing

--- product-webapp/webapp/src/app/app-routing.module.ts
+++ product-webapp/webapp/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './features/login/components/login/login.component';
 import { RegisterComponent } from './features/register/components/register/register.component';
 import { HomeComponent } from './features/home/components/home/home.component';
+import { UserDashBoardComponent } from './features/user-dash-board/user-dash-board.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "login", component: LoginComponent},
-  {path: "register", component: RegisterComponent}
+  {path: "register", component: RegisterComponent},
+  {
+    path:"userDashBoard",
+    component:UserDashBoardComponent
+  }
+  
 ];
 
 @NgModule({
