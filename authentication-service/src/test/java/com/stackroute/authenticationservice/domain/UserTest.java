02764bc511de14ac26adcc4baff0b389a059f976
harshtomar67tb <harshtomar67tb@gmail.com>
2023-05-01 11:03:32 +0530

    test: update domain layer test for user-auth-service

--- authentication-service/src/test/java/com/stackroute/authenticationservice/domain/UserTest.java
+++ authentication-service/src/test/java/com/stackroute/authenticationservice/domain/UserTest.java
@@ -3,18 +3,21 @@ package com.stackroute.authenticationservice.domain;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-@Test
 public class UserTest {
-    // Create a new user
-    User user = new User();
-    user.setFirstName("Mob");
-    user.setLastName("Physcho");
-    user.setEmail("mob100@gmail.com");
-    user.setPassword("mob100");
 
-    // Verify that the user object was created correctly
-    Assertions.assertEquals("Mob", user.getFirstName());
-    Assertions.assertEquals("Physcho", user.getLastName());
-    Assertions.assertEquals("mob100@gmail.com", user.getEmail());
-    Assertions.assertEquals("mob100", user.getPassword());
+    @Test
+    public void testUser() {
+        // Create a new user
+        User user = new User();
+        user.setFirstName("Mob");
+        user.setLastName("Physcho");
+        user.setEmail("mob100@gmail.com");
+        user.setPassword("mob100");
+
+        // Verify that the user object was created correctly
+        Assertions.assertEquals("Mob", user.getFirstName());
+        Assertions.assertEquals("Physcho", user.getLastName());
+        Assertions.assertEquals("mob100@gmail.com", user.getEmail());
+        Assertions.assertEquals("mob100", user.getPassword());
+    }
 }
