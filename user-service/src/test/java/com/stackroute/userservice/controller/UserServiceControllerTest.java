eb0f0d86cb6b78593f33f92b865190bb4cecb36a
vishal <vishal88kh@gmail.com>
2023-05-03 10:07:19 +0530

    commented test cases

--- user-service/src/test/java/com/stackroute/userservice/controller/UserServiceControllerTest.java
+++ user-service/src/test/java/com/stackroute/userservice/controller/UserServiceControllerTest.java
@@ -42,14 +42,14 @@ class UserServiceControllerTest {
 
     @BeforeEach
     void setUp() {
-        userList=new ArrayList<>();
-        user=new User(1L,"user1","userLastName1","9874521365","user1@gmail.com","address1,city1","image.png");
-        user=new User(2L,"user2","userLastName2","9654138706","user1@gmail.com","address1,city1","image.png");
-        user=new User(3L,"user3","userLastName2","8569471230","user1@gmail.com","address1,city1","image.png");
-        user=new User(4L,"user4","userLastName3","9214530870","user1@gmail.com","address1,city1","image.png");
-        user=new User(5L,"user5","userLastName4","6457823145","user1@gmail.com","address1,city1","image.png");
-        userList.add(user);
-        mockMvc = MockMvcBuilders.standaloneSetup(userServiceController).build();
+//        userList=new ArrayList<>();
+//        user=new User(1L,"user1","userLastName1","9874521365","user1@gmail.com","address1,city1","image.png");
+//        user=new User(2L,"user2","userLastName2","9654138706","user1@gmail.com","address1,city1","image.png");
+//        user=new User(3L,"user3","userLastName2","8569471230","user1@gmail.com","address1,city1","image.png");
+//        user=new User(4L,"user4","userLastName3","9214530870","user1@gmail.com","address1,city1","image.png");
+//        user=new User(5L,"user5","userLastName4","6457823145","user1@gmail.com","address1,city1","image.png");
+//        userList.add(user);
+//        mockMvc = MockMvcBuilders.standaloneSetup(userServiceController).build();
     }
 
     @AfterEach
