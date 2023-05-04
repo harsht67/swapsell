a7ffeaa93405094c73ad51a5100ba841395803ec
vishal <vishal88kh@gmail.com>
2023-05-04 10:08:16 +0530

    added method to remove user from application database

--- user-service/src/test/java/com/stackroute/userservice/service/UserServiceTest.java
+++ user-service/src/test/java/com/stackroute/userservice/service/UserServiceTest.java
@@ -1,10 +1,13 @@
 package com.stackroute.userservice.service;
 
 import com.stackroute.userservice.configuration.UserDTO;
+import com.stackroute.userservice.domain.Product;
 import com.stackroute.userservice.domain.User;
+import com.stackroute.userservice.exception.ProductDoesNotExistsException;
 import com.stackroute.userservice.exception.UserAlreadyExistsException;
 import com.stackroute.userservice.exception.UserNotFoundException;
 import com.stackroute.userservice.repository.UserServiceRepository;
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -18,9 +21,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.time.LocalDateTime;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -31,34 +33,20 @@ class UserServiceTest {
     private UserServiceRepository userServiceRepository;
     @InjectMocks
     private UserServiceImpl userService;
-    private User user1,user2;
     private UserDTO userDto;
     List<User> userList;
     @BeforeEach
     void setUp() {
-//        user1=new User(1L,"user1","userLastName",456987123,"user1@gmail.com","address1,city,area-20013",(byte) 2222,"dccsdc",null);
-//        user2=new User(1L,"user2","userLastName",852146397,"user2@gmail.com","address2,city,area-85210",(byte) 45.4,"user2",null);
-//        userDto = new UserDTO();
-//        userList= Arrays.asList(user1,user2);
+
     }
 
     @AfterEach
     void tearDown() {
-        user1=null;
-        user2=null;
         userList=null;
         userServiceRepository.deleteAll();
     }
 
-    @Test
-    @DisplayName("Test for registering user to application")
-    void registerUserToApplication() throws UserAlreadyExistsException {
-//        when(userServiceRepository.findUserByEmail(user1.getEmail())).thenReturn(Optional.empty());
-//        when(userServiceRepository.save(any())).thenReturn(user1);
-////        assertEquals(user1,userService.registerUserToApplication(user1));
-//        verify(userServiceRepository,times(1)).save(any());
-//        verify(userServiceRepository,times(1)).findUserByEmail(any());
-    }
+
 
     @Test
     public void testUpdateUserDetails() throws UserNotFoundException {
@@ -80,6 +68,41 @@ class UserServiceTest {
         User result = userService.updateUserDetails(updatedUser);
 
         assertEquals("456 Maple St", result.getAddress());
+    }
+
+    @Test
+    @DisplayName("Test case for deleting user account")
+    public void deleteUserTest() throws UserNotFoundException {
+        String email = "test@example.com";
+        User user = new User();
+        user.setEmail(email);
+        Mockito.when(userServiceRepository.findUserByEmail(email)).thenReturn(Optional.of(user));
+        boolean result = userService.deleteUser(email);
+        assertTrue(result);
+        Mockito.verify(userServiceRepository, Mockito.times(1)).deleteUserByEmail(email);
+    }
+
+    @Test
+    @DisplayName("test to post add for user")
+    public void testPostAnAdd() throws UserNotFoundException {
+        // Create a sample user and product
+
+        Product product = new Product(1L,"laptop","Test Product", "Test Description",(byte) 10.0,500.00,"test category","good",93,true, null);
+
+        User user = new User(1L,"user1","lastName",15748269,"test@example.com","address city",(byte) 10.2, "password",null);
+
+        // Set up the mock UserServiceRepository to return the sample user when searching by email
+        Mockito.when(userServiceRepository.findUserByEmail(user.getEmail())).thenReturn(Optional.of(user));
+
+        // Call the postAnAdd method with the sample user's email and product
+        User result = userService.postAnAdd(user.getEmail(), product);
+
+        // Verify that the user's product add list contains the new product
+        assertEquals(1, result.getProductAddList().size());
+        assertEquals(product, result.getProductAddList().get(0));
+
+        // Verify that the UserServiceRepository's save method was called with the updated user
+        Mockito.verify(userServiceRepository, Mockito.times(1)).save(result);
     }
 
 
