cec4d24bd3557c1955bf2d558e5307bbae722fb4
vishal <vishal88kh@gmail.com>
2023-05-04 14:12:06 +0530

    added functionality for adding products

--- user-service/src/test/java/com/stackroute/userservice/controller/UserServiceControllerTest.java
+++ user-service/src/test/java/com/stackroute/userservice/controller/UserServiceControllerTest.java
@@ -2,9 +2,12 @@ package com.stackroute.userservice.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.stackroute.userservice.domain.Product;
 import com.stackroute.userservice.domain.User;
 import com.stackroute.userservice.exception.UserAlreadyExistsException;
+import com.stackroute.userservice.exception.UserNotFoundException;
 import com.stackroute.userservice.service.UserServiceImpl;
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,19 +15,24 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static reactor.core.publisher.Mono.when;
@@ -36,21 +44,15 @@ class UserServiceControllerTest {
     @InjectMocks
     UserServiceController userServiceController;
 
+
+
+
+
     MockMvc mockMvc;
     User user;
     List<User> userList;
 
-    @BeforeEach
-    void setUp() {
-//        userList=new ArrayList<>();
-//        user=new User(1L,"user1","userLastName1","9874521365","user1@gmail.com","address1,city1","image.png");
-//        user=new User(2L,"user2","userLastName2","9654138706","user1@gmail.com","address1,city1","image.png");
-//        user=new User(3L,"user3","userLastName2","8569471230","user1@gmail.com","address1,city1","image.png");
-//        user=new User(4L,"user4","userLastName3","9214530870","user1@gmail.com","address1,city1","image.png");
-//        user=new User(5L,"user5","userLastName4","6457823145","user1@gmail.com","address1,city1","image.png");
-//        userList.add(user);
-//        mockMvc = MockMvcBuilders.standaloneSetup(userServiceController).build();
-    }
+
 
     @AfterEach
     void tearDown() {
@@ -69,14 +71,43 @@ class UserServiceControllerTest {
         }
     }
 
+
+
     @Test
-    @DisplayName("Test for user registration")
-    void registerUser() throws Exception {
-//        when((Publisher<?>) userService.registerUserToApplication(any())).thenReturn(user);
-//        mockMvc.perform(post("/registerUser")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                        .content(jsontoString(user))
-//        ).andExpect(status().isCreated())
-//                .andDo(MockMvcResultHandlers.print());
+    public void testUpdateUserDetails_Success() throws UserNotFoundException {
+        // Arrange
+        User user = new User();
+        user.setEmail("test@test.com");
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        user.setPhoneNumber(1234567890);
+        user.setAddress("123 Main St");
+
+        Mockito.when(userService.updateUserDetails(user)).thenReturn(user);
+
+        // Act
+        ResponseEntity<?> response = userServiceController.updateUserDetails(user);
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(user, response.getBody());
     }
+
+    @Test
+    public void testUpdateUserDetails_UserNotFoundException() throws UserNotFoundException {
+        // Arrange
+        User user = new User();
+        user.setEmail("test@test.com");
+
+        Mockito.when(userService.updateUserDetails(user)).thenThrow(new UserNotFoundException("No user found with email id test@test.com"));
+
+        // Act
+        ResponseEntity<?> response = userServiceController.updateUserDetails(user);
+
+        // Assert
+        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+        assertEquals("No user found with email id test@test.com", response.getBody());
+    }
+
+
 }
