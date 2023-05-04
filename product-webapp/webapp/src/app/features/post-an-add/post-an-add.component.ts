e45ba35c636537fa1abe1761d1434451e5cf1941
vishal <vishal88kh@gmail.com>
2023-05-04 15:30:27 +0530

    moved PostAnAdd component to features directory

---
+++ product-webapp/webapp/src/app/features/post-an-add/post-an-add.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-post-an-add',
+  templateUrl: './post-an-add.component.html',
+  styleUrls: ['./post-an-add.component.css']
+})
+export class PostAnAddComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
