82e3e543804bf467dc1331fcc5d0cea0594fe397
harshtomar67tb <harshtomar67tb@gmail.com>
2023-05-08 21:11:07 +0530

    style: product card, feat: sort product result

--- product-webapp/webapp/src/app/features/product-result/product-result.component.ts
+++ product-webapp/webapp/src/app/features/product-result/product-result.component.ts
@@ -11,8 +11,21 @@ export class ProductResultComponent {
   products = [1,2,3,4,5,6,7,8,9];
 
   dropdown: boolean = false;
+  dropdownValues = [
+    "what's new",
+    "recommended",
+    "price - low to high",
+    "price - high to low"
+  ]
+  sort: string = "recommended"; 
 
   filter: boolean = true;
+
+  // change sort value
+  updateSort(newSort: string) {
+    this.sort = newSort;
+    this.toggleDropdown();
+  }
 
   // open/close sort-dropdown
   toggleDropdown() {
