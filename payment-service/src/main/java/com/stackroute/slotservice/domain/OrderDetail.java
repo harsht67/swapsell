9233f0aa8c7f6ff3d1e548a6d5a79793eebe197f
vishal <vishal88kh@gmail.com>
2023-05-10 15:52:10 +0530

    added fields for OrderDetail

--- payment-service/src/main/java/com/stackroute/slotservice/domain/OrderDetail.java
+++ payment-service/src/main/java/com/stackroute/slotservice/domain/OrderDetail.java
@@ -1,4 +1,16 @@
 package com.stackroute.slotservice.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Document
 public class OrderDetail {
+    @MongoId
+    private Integer orderId;
+    private String userFullName;
+    private String addressToDeliver;
+    private String contactNumber;
+    private String orderStatus;
+    private double orderAmount;
 }
