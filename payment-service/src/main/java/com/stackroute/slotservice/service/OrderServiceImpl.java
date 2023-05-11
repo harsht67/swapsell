b1849f56c8e02653f8b6225f12bdd84d14ad1a32
vishal <vishal88kh@gmail.com>
2023-05-11 09:53:21 +0530

    added userRepository as dependency injection

--- payment-service/src/main/java/com/stackroute/slotservice/service/OrderServiceImpl.java
+++ payment-service/src/main/java/com/stackroute/slotservice/service/OrderServiceImpl.java
@@ -3,12 +3,17 @@ package com.stackroute.slotservice.service;
 
 import com.stackroute.slotservice.domain.User;
 import com.stackroute.slotservice.exception.UserAlreadyExistsException;
+import com.stackroute.slotservice.repository.UserRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+    private final UserRepository userRepository;
+
+    public OrderServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public User addUserToDataBase(String emailId) throws UserAlreadyExistsException {
 
