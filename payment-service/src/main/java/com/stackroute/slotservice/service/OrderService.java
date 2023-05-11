d07a9fc8d49f1aabc70ec47cef0ee0618f2e19dc
vishal <vishal88kh@gmail.com>
2023-05-11 09:45:14 +0530

    added a method to save the user details in database

--- payment-service/src/main/java/com/stackroute/slotservice/service/OrderService.java
+++ payment-service/src/main/java/com/stackroute/slotservice/service/OrderService.java
@@ -1,7 +1,10 @@
 package com.stackroute.slotservice.service;
 
 import com.stackroute.slotservice.domain.OrderInput;
+import com.stackroute.slotservice.domain.User;
+import com.stackroute.slotservice.exception.UserAlreadyExistsException;
 
 public interface OrderService {
+    User addUserToDataBase(String emailId) throws UserAlreadyExistsException;
 
 }
--- payment-service/src/main/java/com/stackroute/slotservice/service/OrderServiceImpl.java
+++ payment-service/src/main/java/com/stackroute/slotservice/service/OrderServiceImpl.java
@@ -1,15 +1,17 @@
 package com.stackroute.slotservice.service;
 
-import com.stackroute.slotservice.domain.OrderDetail;
-import com.stackroute.slotservice.domain.OrderInput;
-import com.stackroute.slotservice.domain.Product;
-import com.stackroute.slotservice.filter.JwtFilter;
-import com.stackroute.slotservice.repository.OrderDetailRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import com.stackroute.slotservice.domain.User;
+import com.stackroute.slotservice.exception.UserAlreadyExistsException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+    @Override
+    public User addUserToDataBase(String emailId) throws UserAlreadyExistsException {
+
+        return null;
+    }
 }
