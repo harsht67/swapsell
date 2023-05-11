1629575edb6f48bd4764a84919ea688cfa29e959
vishal <vishal88kh@gmail.com>
2023-05-11 10:05:59 +0530

    added a method in the controller so this will map the request with method

--- payment-service/src/main/java/com/stackroute/slotservice/controller/UserController.java
+++ payment-service/src/main/java/com/stackroute/slotservice/controller/UserController.java
@@ -1,14 +1,33 @@
 package com.stackroute.slotservice.controller;
 
+import com.stackroute.slotservice.domain.User;
+import com.stackroute.slotservice.exception.UserAlreadyExistsException;
 import com.stackroute.slotservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/swapSell")
 public class UserController {
     private final UserService userService;
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/userTransactionRegister")
+    public ResponseEntity<?> registerUser(@RequestBody User user){
+        try {
+            User addUserToDataBase = userService.addUserToDataBase(user.getEmailId());
+            return new ResponseEntity<>(addUserToDataBase, HttpStatus.CREATED);
+        } catch (UserAlreadyExistsException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+
+        }
     }
 }
