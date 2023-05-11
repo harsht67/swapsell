8f8e36476e80217b81553ace9d878b4081867b0f
vishal <vishal88kh@gmail.com>
2023-05-11 09:52:56 +0530

    changed interface name to UserRepository

--- payment-service/src/main/java/com/stackroute/slotservice/repository/UserRepository.java
+++ payment-service/src/main/java/com/stackroute/slotservice/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package com.stackroute.slotservice.repository;
 
-import com.stackroute.slotservice.domain.OrderDetail;
 import com.stackroute.slotservice.domain.User;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderDetailRepository  extends MongoRepository<User,String> {
+public interface UserRepository extends MongoRepository<User,String> {
     User findUserByEmailId(String emailId);
 }
