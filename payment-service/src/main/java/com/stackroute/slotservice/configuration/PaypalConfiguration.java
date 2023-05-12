a5b5d0e4aec771fb2a8798a31a33dba74719dc2d
vishal <vishal88kh@gmail.com>
2023-05-12 15:15:11 +0530

    added method for configuration

--- payment-service/src/main/java/com/stackroute/slotservice/configuration/PaypalConfiguration.java
+++ payment-service/src/main/java/com/stackroute/slotservice/configuration/PaypalConfiguration.java
@@ -1,4 +1,27 @@
 package com.stackroute.slotservice.configuration;
 
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class PaypalConfiguration {
+    @Value("${paypal.client.id}")
+    private String clientId;
+    @Value("${paypal.client.secret}")
+    private String clientSecret;
+    @Value("${paypal.mode}")
+    private String mode;
+
+    @Bean
+    public Map<String,String> payPalConfig(){
+        Map<String,String> configMap = new HashMap<>();
+        configMap.put("mode",mode);
+        return configMap;
+    }
+
 }
