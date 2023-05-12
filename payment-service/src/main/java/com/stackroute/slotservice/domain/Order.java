4e6b5ad843f38e447ae1937f23325b495a032c3a
vishal <vishal88kh@gmail.com>
2023-05-12 16:25:35 +0530

    added all the required annotations for order class

--- payment-service/src/main/java/com/stackroute/slotservice/domain/Order.java
+++ payment-service/src/main/java/com/stackroute/slotservice/domain/Order.java
@@ -1,4 +1,17 @@
 package com.stackroute.slotservice.domain;
 
+import lombok.*;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Data
+@ToString
 public class Order {
+    private double price;
+    private String method;
+    private String intent;
+    private String description;
 }
