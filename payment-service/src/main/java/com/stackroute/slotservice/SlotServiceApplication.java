4e56dcda26613ea1c9303102c8a68ebed50cce9f
vishal <vishal88kh@gmail.com>
2023-05-12 14:59:05 +0530

    removed all previous code

--- payment-service/src/main/java/com/stackroute/slotservice/SlotServiceApplication.java
+++ payment-service/src/main/java/com/stackroute/slotservice/SlotServiceApplication.java
@@ -1,10 +1,8 @@
 package com.stackroute.slotservice;
 
-import com.stackroute.slotservice.filter.JwtFilter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SlotServiceApplication {
@@ -13,11 +11,5 @@ public class SlotServiceApplication {
 		SpringApplication.run(SlotServiceApplication.class, args);
 	}
 
-	@Bean
-	public FilterRegistrationBean<JwtFilter> filterRegistrationBean(){
-		FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<>();
-		registrationBean.setFilter(new JwtFilter());
-		registrationBean.addUrlPatterns("/userAuth/*");
-		return registrationBean;
-	}
+
 }
--- payment-service/src/main/java/com/stackroute/slotservice/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.stackroute.slotservice.service;
-
-import com.stackroute.slotservice.domain.User;
-import com.stackroute.slotservice.exception.UserAlreadyExistsException;
-
-public interface UserService {
-    User addUserToDataBase(User user) throws UserAlreadyExistsException;
-
-}
