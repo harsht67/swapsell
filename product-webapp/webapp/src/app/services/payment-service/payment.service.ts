fc25bd05d455a242d08ea93c4b2107e31588e5a0
vishal <vishal88kh@gmail.com>
2023-05-14 18:15:55 +0530

    added a method to post an order

--- product-webapp/webapp/src/app/services/payment-service/payment.service.ts
+++ product-webapp/webapp/src/app/services/payment-service/payment.service.ts
@@ -1,12 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Order } from 'src/app/modals/Order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentService {
 
+  baseUrl:string="http://localhost:8080/payment/";
   constructor(private httpClient:HttpClient) { }
 
-  // createOrder()
+  createOrder(orderData:Order):Observable<Order>{
+    return this.httpClient.post<Order>(`${this.baseUrl}/pay`,orderData);
+  }
 }
