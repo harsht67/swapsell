b98345a25e44933ce062479fa2a19edbf06ebc59
vishal <vishal88kh@gmail.com>
2023-05-15 11:45:21 +0530

    fixed the error , changed the payerId to paymentId

--- payment-service/src/main/java/com/stackroute/slotservice/service/PayPalServiceImpl.java
+++ payment-service/src/main/java/com/stackroute/slotservice/service/PayPalServiceImpl.java
@@ -25,37 +25,48 @@ public class PayPalServiceImpl implements PayPalService {
             String description,
             String cancelUrl,
             String successUrl) throws PayPalRESTException {
+        System.out.println("In payment service");
         Amount amount = new Amount();
         amount.setCurrency(currency);
         total = new BigDecimal(total).setScale(2, RoundingMode.HALF_UP).doubleValue();
         amount.setTotal(String.format("%.2f",total));
 
+        System.out.println("total amount "+total);
+
+
         Transaction transaction = new Transaction();
         transaction.setDescription(description);
         transaction.setAmount(amount);
+
+        System.out.println("transaction is "+transaction);
+
 
         List<Transaction> transactionList = new ArrayList<>();
         transactionList.add(transaction);
 
         Payer payer = new Payer();
         payer.setPaymentMethod(method);
-
         Payment payment = new Payment();
         payment.setIntent(intent);
         payment.setPayer(payer);
+        System.out.println("payment is here");
+        System.out.println(payment);
         payment.setTransactions(transactionList);
         RedirectUrls redirectUrls = new RedirectUrls();
         redirectUrls.setCancelUrl(cancelUrl);
         redirectUrls.setReturnUrl(successUrl);
         payment.setRedirectUrls(redirectUrls);
+        System.out.println("we are here"+payment.create(apiContext));
         return payment.create(apiContext);
     }
 
     public Payment executePayment(String paymentId,String payerId) throws PayPalRESTException {
         Payment payment = new Payment();
-        payment.setId(payerId);
+        payment.setId(paymentId);
         PaymentExecution  paymentExecution  = new PaymentExecution();
         paymentExecution.setPayerId(payerId);
+        System.out.println("payment execution");
+        System.out.println(paymentExecution);
         return payment.execute(apiContext,paymentExecution);
     }
 
