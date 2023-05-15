801a354b76c6a6b96fc8232d527fcb10bae02f2f
vishal <vishal88kh@gmail.com>
2023-05-15 17:28:43 +0530

    added a method in service layer to update the transactions list in user document

--- payment-service/src/main/java/com/stackroute/slotservice/service/UserServiceImpl.java
+++ payment-service/src/main/java/com/stackroute/slotservice/service/UserServiceImpl.java
@@ -1,9 +1,15 @@
 package com.stackroute.slotservice.service;
 
+import com.paypal.api.payments.Payment;
 import com.stackroute.slotservice.domain.User;
 import com.stackroute.slotservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -16,5 +22,24 @@ public class UserServiceImpl implements UserService {
     @Override
     public User saveUserData(User user) {
         return userRepository.save(user);
+    }
+
+    @Override
+    public void UpdateUserTransaction(String userId, Payment payment) {
+        Optional<User> userById = userRepository.findById(userId);
+        User user;
+        if (userById.isPresent()){
+            System.out.println("user found "+userById);
+            user = userById.get();
+            List<Payment> paymentList = user.getPaymentList();
+            if (paymentList==null){
+                user.setPaymentList(Collections.singletonList(payment));
+            }else {
+                paymentList.add(payment);
+                user.setPaymentList(paymentList);
+            }
+            userRepository.save(user);
+
+        }
     }
 }
