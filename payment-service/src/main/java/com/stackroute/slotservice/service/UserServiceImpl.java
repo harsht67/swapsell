9c7c05536c9914afb482d5a51707e62f92d925ec
vishal <vishal88kh@gmail.com>
2023-05-15 15:45:10 +0530

    added UserRepository as dependency injection

--- payment-service/src/main/java/com/stackroute/slotservice/service/UserServiceImpl.java
+++ payment-service/src/main/java/com/stackroute/slotservice/service/UserServiceImpl.java
@@ -1,10 +1,18 @@
 package com.stackroute.slotservice.service;
 
 import com.stackroute.slotservice.domain.User;
+import com.stackroute.slotservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public User saveUserData(User user) {
         return null;
