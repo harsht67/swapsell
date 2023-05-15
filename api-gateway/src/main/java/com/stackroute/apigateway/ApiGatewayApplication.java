162eab67a6f2456eeecd93c9f7c7a65d3c261acb
vishal <vishal88kh@gmail.com>
2023-05-15 16:35:08 +0530

    added a route for userPayment url

--- api-gateway/src/main/java/com/stackroute/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/stackroute/apigateway/ApiGatewayApplication.java
@@ -16,6 +16,7 @@ public class ApiGatewayApplication {
 	public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder){
 		return routeLocatorBuilder.routes()
 				.route(p->p.path("/pay/**").uri("http://localhost:8084/"))
+				.route(p->p.path(("/userPayment/**")).uri("http://locahost:8084/"))
 				.build();
 	}
 }
