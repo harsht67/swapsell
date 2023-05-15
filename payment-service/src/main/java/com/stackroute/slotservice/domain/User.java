da7cfe310ff4109fe9e22a24d3beb00c363252b6
vishal <vishal88kh@gmail.com>
2023-05-15 15:00:20 +0530

    added user class and UserRepository

--- payment-service/src/main/java/com/stackroute/slotservice/domain/User.java
+++ payment-service/src/main/java/com/stackroute/slotservice/domain/User.java
@@ -1,4 +1,18 @@
 package com.stackroute.slotservice.domain;
 
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Document
 public class User {
+    @MongoId
+    private String emailId;
 }
