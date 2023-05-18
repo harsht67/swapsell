b9caa067046f499aa94f02b998889520cfc58c3a
vishal <vishal88kh@gmail.com>
2023-05-18 15:46:48 +0530

    added all required annotations in MainResponse

--- email-service/src/main/java/com/stackroute/emailservice/dto/MailResponse.java
+++ email-service/src/main/java/com/stackroute/emailservice/dto/MailResponse.java
@@ -1,5 +1,14 @@
 package com.stackroute.emailservice.dto;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class MailResponse {
     private String message;
     private boolean status;
