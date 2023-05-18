fde0b0c246a3cb3e68365f863026ece5f4dd20ce
vishal <vishal88kh@gmail.com>
2023-05-18 15:48:15 +0530

    added all required annotaions

--- email-service/src/main/java/com/stackroute/emailservice/dto/MailRequest.java
+++ email-service/src/main/java/com/stackroute/emailservice/dto/MailRequest.java
@@ -1,5 +1,14 @@
 package com.stackroute.emailservice.dto;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode
+@ToString
 public class MailRequest {
     private String name;
     private String to;
