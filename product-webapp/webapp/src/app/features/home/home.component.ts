403850a63faa67b369e829d3e8fd1a90a29e04c9
harshtomar67tb <harshtomar67tb@gmail.com>
2023-05-19 14:19:35 +0530

    add routing - home to product page

--- product-webapp/webapp/src/app/features/home/home.component.ts
+++ product-webapp/webapp/src/app/features/home/home.component.ts
@@ -1,26 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Product } from "src/app/modals/product";
+import { ProductService } from "src/app/services/product.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent {
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.fetchProducts();
+    this.productService.products$.subscribe(products => {
+      this.products = products;
+    });
+  }
 
   categories1 = [
     "Vehicles",
     "Real Estate",
     "Electronics & Appliances",
-    "Home & Furniture"
+    "Home & Furniture",
   ];
-
   categories2 = [
     "Books, Sports & Hobbies",
     "Mobiles",
     "Computers & Laptops",
-    "Office"
-  ]
-  
-  products = [1,2,3,4];
+    "Office",
+  ];
 
+  // products = [1, 2, 3, 4];
 }
--- product-webapp/webapp/src/app/services/product.service.ts
+++ product-webapp/webapp/src/app/services/product.service.ts
@@ -1,18 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Product } from '../modals/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  URL = "http://localhost:3000/products";
+  private productsSubject: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
+  public products$: Observable<Product[]> = this.productsSubject.asObservable();
+
+  URL = "http://localhost:9090/products";
 
   constructor(private http: HttpClient) { }
 
-  getProducts(): Observable<any> {
-    return this.http.get(this.URL);
+  // fetch all products
+  fetchProducts(): void {
+    this.http.get<Product[]>(this.URL).subscribe(
+      (products: Product[]) => {
+        this.productsSubject.next(products);
+      },
+      (error) => {
+        console.error('Error fetching products:', error);
+      }
+    );
+  }
+
+  // returns a single product by id 
+  getProductById(productId: string): Observable<Product> {
+    console.log(productId);
+    return this.products$.pipe(
+      map(products => products.find(product => product.id == productId))
+    );
   }
 
 }
