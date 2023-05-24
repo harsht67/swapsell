0bd8aa565141f71d0be5db0cfb8efeab11c5a947
harshtomar67tb <harshtomar67tb@gmail.com>
2023-05-24 18:31:29 +0530

    latest code

--- product-service/src/main/java/com/stackroute/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/stackroute/productservice/ProductServiceApplication.java
@@ -3,8 +3,10 @@ package com.stackroute.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableNeo4jRepositories("com/stackroute/productservice/repository")
 public class ProductServiceApplication {
 
--- user-service/src/main/java/com/stackroute/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/stackroute/userservice/UserServiceApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
